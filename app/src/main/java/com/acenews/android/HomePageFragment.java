156852602b843c2bdfe9c3a22de3aaefa9f24d7d
yixin <1377945242@qq.com>
2019-12-24 13:12:05 +0800

    second commit

--- app/src/main/java/com/acenews/android/HomePageFragment.java
+++ app/src/main/java/com/acenews/android/HomePageFragment.java
@@ -1,14 +1,27 @@
 package com.acenews.android;
 
 
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -16,10 +29,11 @@ import android.view.ViewGroup;
  */
 public class HomePageFragment extends Fragment {
 
-
     public HomePageFragment() {
         // Required empty public constructor
     }
+    private TabLayout mTabLayout;
+    private ViewPager2 mViewPager2;
 
 
     @Override
@@ -30,9 +44,38 @@ public class HomePageFragment extends Fragment {
     }
 
 
+
+
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        
+        mViewPager2 = getActivity().findViewById(R.id.newsViewPage);
+        mTabLayout = getActivity().findViewById(R.id.tabLayout);
+        mViewPager2.setAdapter(new FragmentStateAdapter(this) {
+            @NonNull
+            @Override
+            public Fragment createFragment(int position) {
+                final List<NewsFragment> newsFragmentList = new ArrayList<>();
+                for(int i = 0; i < 3; i++ ){
+                    newsFragmentList.add(new NewsFragment());
+                }
+                if(position == 1){
+                    return new MyFragment();
+                }
+                return newsFragmentList.get(position);
+            }
+            @Override
+            public int getItemCount() {  //返回三个页面
+                return 3;
+            }
+        });
+        TabLayoutMediator tabLayoutMediator = new TabLayoutMediator(mTabLayout, mViewPager2, new TabLayoutMediator.TabConfigurationStrategy() {
+            @Override
+            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                tab.setText("Fragment" + position);
+            }
+        });
+        tabLayoutMediator.attach();
+
     }
 }
--- app/src/main/java/com/acenews/android/NewsFragment.java
+++ app/src/main/java/com/acenews/android/NewsFragment.java
@@ -8,12 +8,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class NewsFragment extends Fragment {
+
 
 
     public NewsFragment() {
@@ -26,6 +28,7 @@ public class NewsFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_news, container, false);
+
     }
 
 }
